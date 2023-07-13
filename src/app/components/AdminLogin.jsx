54542fbbf8a63673c0237c28374ac7599436106a
IzaanD98 <izzy.dalvi@gmail.com>
2023-07-13 16:07:31 +0100

    changes to auth

--- src/app/components/AdminLogin.jsx
+++ src/app/components/AdminLogin.jsx
@@ -14,7 +14,7 @@ const AdminLogin = ({ authenticated }) => {
 
     if (username === process.env.ADMIN && password === process.env.PASSWORD) {
       localStorage.setItem("isAdminLoggedIn", true);
-      router.push("/applicants");
+      return router.push("/applicants");
     } else {
       setErrorMessage("Invalid username or password");
     }
