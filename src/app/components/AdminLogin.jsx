9efe81f5e6c681c9816357bfaa8d6f6d728dc726
IzaanD98 <izzy.dalvi@gmail.com>
2023-07-13 15:52:56 +0100

    changes to auth

--- src/app/components/AdminLogin.jsx
+++ src/app/components/AdminLogin.jsx
@@ -22,7 +22,7 @@ const AdminLogin = ({ authenticated }) => {
   return (
     <div className="bg-gray-200 flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold mb-8">Admin Login</h1>
-      <form className="w-64" onClick={handleLogin}>
+      <form className="w-64" onSubmit={handleLogin}>
         <div className="mb-4">
           <label className="text-lg">Username:</label>
           <input
